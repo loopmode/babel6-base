048c0285d399856132ffab8e23ae6476491c1e4e
Jovica Aleksic <jovica.aleksic@xailabs.de>
2018-08-14 10:12:20 +0200

    display warning and throw when package.json is missing

--- cli/index.js
+++ cli/index.js
@@ -1,4 +1,6 @@
 #!/usr/bin/env node
+const fs = require('fs');
+const path = require('path');
 const program = require('commander');
 const { prompt } = require('inquirer');
 
@@ -9,6 +11,16 @@ program
     .alias('i')
     .description('Add boilerplate files and scripts to your project')
     .action(function(dir = process.cwd()) {
+        if (!fs.existsSync(path.resolve(dir, 'package.json'))) {
+            console.warn(`
+    ${path.resolve(dir)} doesn't seem to be a node.js project.
+
+    Please initialize first using "npm init" or "yarn init".
+    `);
+            throw new Error('No package.json found');
+            process.exit(1);
+        }
+
         prompt(install.questions).then(options => install(dir, options));
     });
 
